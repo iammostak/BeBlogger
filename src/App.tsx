f02e0c36c7ae9c4b78c4f866fe1ee06290e8815a
iammostak <mdmostafijur85@gmail.com>
2022-10-14 23:47:39 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Container } from "@chakra-ui/react";
+import "./App.css";
+
+function App() {
+
+   return (
+      <Container maxW={'container'} className="App">
+         medium.com
+      </Container>
+   );
+}
+
+export default App;
